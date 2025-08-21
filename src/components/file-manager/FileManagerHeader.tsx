97026d77625b1149db0a86ef250155f652ff6a6e
saapro-lang <rui@saapro.net>
2025-08-21 15:44:09 +1000

    Updated FileManagerHeader.tsx

--- src/components/file-manager/FileManagerHeader.tsx
+++ src/components/file-manager/FileManagerHeader.tsx
@@ -187,7 +187,7 @@ export function FileManagerHeader({
           
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button size="sm" className="gap-2">
+              <Button size="sm" className="gap-2 bg-[#605BFF]">
                 <Plus className="h-4 w-4" />
                 New
               </Button>
